fe0a3f7a9a9d0e8b9c4dbd243cdee6903c4d3c05
Alexnet11 <borodatyyne@gmail.com>
2026-08-22 22:19:58 +0300

    WIP: continue movie library improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { Route, Routes} from 'react-router';
-import NotFoundPage from './pages/NotFoundPage';
-import Header from './components/Header'; 
-import HomePage from './pages/HomePage';
+import React from 'react';
 import MoviesPage from './pages/MoviesPage';
 import './App.css';
 
+
 function App() {
   return(
-    <>
-      <Header/>
-
-      <main className='main-content'>
-        <Routes>
-          <Route
-            path='/'
-            element = {<HomePage/>} 
-          />
-
-          <Route
-            path='/movies'
-            element= {<MoviesPage/>} 
-          />
-
-          <Route
-            path='/movies/:movieId'
-            element={<MoviesPage/>}
-          />
-
-          <Route
-            path='*'
-            element = {<NotFoundPage/>}
-          />
-        </Routes>
-      </main>
-    </>
+    <div>
+      <h1>Бтблиотека фильмов</h1>
+      <MoviesPage/>
+    </div>
   );
 }
 
